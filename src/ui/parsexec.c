0e9bee9a8fa9fb00a84ca3c5107dd4321b297d68
Kristina Siri Steinström <kristina.siri.steinstroem@fu-berlin.de>
2026-03-18 17:14:10 +0100

    moved ui files into own module folder

--- src/ui/parsexec.c
+++ src/ui/parsexec.c
@@ -1,19 +1,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <locations.h>
 
 bool parseAndExecute(char *input){
-    char *verb = strtok(input, "\n");
-    char *noun = strtok(NULL, "\n");
+    char *verb = strtok(input, " \n");
+    char *noun = strtok(NULL, " \n");
 
     if (verb != NULL){
         if (strcmp(verb, "quit") == 0){
             return 0;
-        }else if (strcmp(verb, "look") == 0){
-        printf("It is very dark in here.\n");
+        }
+        else if (strcmp(verb, "look") == 0){
+            executeLook(noun);
         }
         else if (strcmp(verb,"go") == 0){
-            printf("You can't see where you're going!\n");
+            executeGo(noun);
         }
         else{
             printf("I don't know how to '%s'.\n", verb);
