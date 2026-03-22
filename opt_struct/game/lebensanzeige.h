76d1c426b19f7cc1dd5130f2f7d4040dfbb15515
Marvin Beutler <marvin.beu97@outlook.de>
2026-03-23 00:58:44 +0100

    Makros für die Lebensanzeigen von 0 bis 100

---
+++ opt_struct/game/lebensanzeige.h
@@ -0,0 +1,106 @@
+// Makros für die Lebensanzeigen von 0 bis 100
+
+#define hp0 \
+normal weißH " " schwarz "Leben: 0" "                                                                             \n" \
+" " grünH "" schwarzH "                    " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp5 \
+normal weißH " " schwarz "Leben: 5" "                                                                             \n" \
+" " grünH " " schwarzH "                   " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp10 \
+normal weißH " " schwarz "Leben: 10" "                                                                            \n" \
+" " grünH "  " schwarzH "                  " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp15 \
+normal weißH " " schwarz "Leben: 15" "                                                                            \n" \
+" " grünH "   " schwarzH "                 " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp20 \
+normal weißH " " schwarz "Leben: 20" "                                                                            \n" \
+" " grünH "    " schwarzH "                " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp25 \
+normal weißH " " schwarz "Leben: 25" "                                                                            \n" \
+" " grünH "     " schwarzH "               " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp30 \
+normal weißH  " " schwarz "Leben: 30" "                                                                            \n" \
+" " grünH "      " schwarzH "              " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp35 \
+normal weißH " " schwarz "Leben: 35" "                                                                            \n" \
+" " grünH "       " schwarzH "             " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp40 \
+normal weißH " " schwarz "Leben: 40" "                                                                            \n" \
+" " grünH "        " schwarzH "            " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp45 \
+normal weißH " " schwarz "Leben: 45" "                                                                            \n" \
+" " grünH "         " schwarzH "           " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp50 \
+normal weißH " " schwarz "Leben: 50" "                                                                            \n" \
+" " grünH "          " schwarzH "          " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp55 \
+normal weißH " " schwarz "Leben: 55" "                                                                            \n" \
+" " grünH "           " schwarzH "         " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp60 \
+normal weißH " " schwarz "Leben: 60" "                                                                            \n" \
+" " grünH "            " schwarzH "        " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp65 \
+normal weißH " " schwarz "Leben: 65" "                                                                            \n" \
+" " grünH "             " schwarzH "       " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp70 \
+normal weißH " " schwarz "Leben: 70" "                                                                            \n" \
+" " grünH "              " schwarzH "      " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp75 \
+normal weißH " " schwarz "Leben: 75" "                                                                            \n" \
+" " grünH "               " schwarzH "     " weißH "                                                                 \n"  \
+"                                                                                      " normal "\n"
+
+#define hp80 \
+normal weißH " " schwarz "Leben: 80" "                                                                            \n" \
+" " grünH "                " schwarzH "    " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp85 \
+normal weißH " " schwarz "Leben: 85" "                                                                            \n" \
+" " grünH "                 " schwarzH "   " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp90 \
+normal weißH " " schwarz "Leben: 90" "                                                                            \n" \
+" " grünH "                  " schwarzH "  " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp95 \
+normal weißH " " schwarz "Leben: 95" "                                                                            \n" \
+" " grünH "                   " schwarzH " " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
+
+#define hp100 \
+normal weißH " " schwarz "Leben: 100" "                                                                           \n" \
+" " grünH "                    " weißH "                                                                 \n" \
+"                                                                                      " normal "\n"
