c9bb65729daf16ca50d49165600cd4324ab483f6
Marvin Beutler <marvin.beu97@outlook.de>
2026-03-23 01:05:36 +0100

    Spielmenü Programm + Quellcode

---
+++ opt_struct/game/menu.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "farben.h" // eigener Header mit Makros für die Farben
+#include "menu.h" // eigener Header mit Makros für die Textformatierung der Menüs
+#include "lebensanzeige.h" // eigener Header mit Makros, für die Darstellung der verschiedenen Lebensanzeigen
+#include <stdlib.h>
+#include <stddef.h>
+
+int main(void)
+{
+char Auswahl;
+char Spielername[15];
+char* hp;
+
+printf(löschen);
+Startmenu:
+printf(Spielmenu);
+printf(normal "\nWas möchtest du tun? ");
+scanf(" %c", &Auswahl);
+
+	if (Auswahl == '1') {
+		Schwierigkeit:
+		printf(löschen);
+		printf(SpielSchwierigkeit);
+		printf("\nIch wähle ");
+		scanf(" %c", &Auswahl);
+
+		if (Auswahl == '1') {
+			hp = hp100;
+			}
+
+		else if (Auswahl == '2') {
+			hp = hp75;
+			}
+
+		else if (Auswahl == '3') {
+			hp = hp50;
+			}
+
+		else {
+			printf(löschen);
+			printf(SpielmenuFalscheEingabe);
+			goto Schwierigkeit;
+			}
+	}
+
+	else if (Auswahl == '2') {
+		printf(löschen);
+		printf(SpielBeenden);
+		exit(0);
+	}
+
+	else {
+		printf(löschen);
+		printf(SpielmenuFalscheEingabe);
+		goto Startmenu;
+	}
+
+Spielstart:
+printf(löschen);
+printf(SpielerName);
+printf("\nMein Name ist ");
+scanf(" %14s", Spielername);
+printf(löschen);
+printf(SpielStart, Spielername);
+printf(Lebensbalken, hp);
+
+return (0);
+}
+
