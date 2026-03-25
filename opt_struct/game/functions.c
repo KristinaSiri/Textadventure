fe417628f654db6c5c25752f6bbb1ac78f118fd7
nosaayri-cyber <urmom@DESKTOP-PJR0H01.localdomain>
2026-03-25 23:17:13 +0100

    erste erweiterungen

--- opt_struct/game/functions.c
+++ opt_struct/game/functions.c
@@ -21,14 +21,24 @@ Item* env_openInventory(GameState *gs);
 enum {
 
     LOC_START = 0,
-    LOC_CHAMBER,
+    LOC_MAIN_DRIVEWAY,
+    LOC_GATEHOUSE,
+    LOC_MAIN_ENTRANCE,
+    LOC_GATEHOUSE_OFFICE,
+    LOC_GATEHOUSE_CLOSET,
+    LOC_GATEHOUSE_GUARDSHACK,
     LOC_COUNT
 };
 
 enum { 
 
-    DOOR_START_CHAMBER, 
-    DOOR_COUNT 
+    ENT_PATH_,
+    ENT_DRIVEWAY_GATEHOUSE,
+    ENT_GATEHOUSE_MAIN,
+    ENT_GATEHOUSE_OFFICE,
+    D5,
+    DOOR_COUNT
+    
 };
 
 enum { 
@@ -41,14 +51,22 @@ enum {
 enum { 
 
     NPC_MINER, 
-    NPC_GUARD, 
+    NPC_GUARD,
+    NPC_GRANNY,
     NPC_COUNT 
 };
 
 enum {
     ITM_LEER = 0,
     ITM_TEST_ZETTEL,
+    ITM_GRANNYS_PURSE,
+    ITM_GRANNYS_GLASSES,
+
+
     KEY_TEST,
+    KEY_GATEH_MAIN,
+
+
     ITM_COUNT
 };
 
@@ -77,30 +95,143 @@ enum {
 
 
 static  Person personPool[NPC_COUNT];
+static  Door doorPool[DOOR_COUNT];
+static  Item itemPool[ITM_COUNT];
+static  Compartment compPool[COMP_COUNT];
+static  Objekt objectPool[OBJ_COUNT];
+static  Person personPool[NPC_COUNT];
+static  Location worldData[LOC_COUNT];
+
+
 
 
 static Door doorPool[DOOR_COUNT] = {
 
     
-    [DOOR_START_CHAMBER] = {
+    [ENT_PATH_] = {
         .name = "stone arch",
-        .description = "A rough archway connects cave and chamber.",
-        .targetIndex = { LOC_START, LOC_CHAMBER },
+        .description = "A rough, stoneren archway marks the entrance to an astate.",
+        .targetIndex = { LOC_START, LOC_MAIN_DRIVEWAY },
         .actions    = {
             {.name = "open door", .execute = act_openDoor},
-            {.name = "use item", .execute = act_giveItem}
+            {.name = "use item", .execute = act_giveItem, .token = { .Tkns = { &doorPool[ENT_PATH_].wants } } }
+        },
+
+        .wants = {&itemPool[KEY_TEST]},
+        .wantsCount = 1,
+
+        .actionCount = 2,
+        .locked = false,
+
+        .sat = {
+            .level = 1, 
+            .satActions = {
+                {.execute = sat_UnOrLock, .token = { .Tkns = { &doorPool[ENT_PATH_].locked } } }
+            }, 
+            .satActionCount = 1
+        }
+    },
+
+    [ENT_DRIVEWAY_GATEHOUSE] = {
+        .name = "ENT_DRIVEWAY_GATEHOUSE",
+        .description = "Test desc door 2",
+
+        .targetIndex = { LOC_MAIN_DRIVEWAY, LOC_GATEHOUSE },
+        .actions    = {
+            {.name = "open door", .execute = act_openDoor}
         },
         .actionCount = 1,
+        
+        .wants = { },
+        .wantsCount = 0,
+
+        
+        .locked = false,
+
+
+    },
+
+    [ENT_GATEHOUSE_MAIN] = {
+        .name = "ENT_GATEHOUSE_MAIN",
+        .description = "Test desc door 3",
+
+        .targetIndex = { LOC_GATEHOUSE, LOC_MAIN_ENTRANCE },
+
+        .actions    = {
+            {.name = "open door", .execute = act_openDoor},
+            {.name = "use item", .execute = act_giveItem, .token = { .Tkns = { &doorPool[ENT_GATEHOUSE_MAIN].wants } } }
+        },
+
+        .wants = {&itemPool[KEY_GATEH_MAIN]},
+        .wantsCount = 1,
+
+        .actionCount = 2,
         .locked = true,
 
         .sat = {
             .level = 1, 
             .satActions = {
-                {.execute = sat_UnOrLock, .token = { .Tkns = { &doorPool[DOOR_START_CHAMBER].locked } }}
+                {.execute = sat_UnOrLock, .token = { .Tkns = { &doorPool[ENT_GATEHOUSE_MAIN].locked } } }
             }, 
             .satActionCount = 1
         }
-    }
+    },
+
+    [ENT_GATEHOUSE_OFFICE] = {
+        .name = "ENT_GATEHOUSE_OFFICE",
+        .description = "Test desc door 3",
+
+        .targetIndex = { LOC_GATEHOUSE, LOC_GATEHOUSE_OFFICE },
+
+        .actions    = {
+            {.name = "open door", .execute = act_openDoor}
+            
+        },
+
+
+
+        .actionCount = 1,
+        .locked = false
+
+        
+    },
+
+    [] = {
+        .name = "",
+        .description = "Test desc door 3",
+
+        .targetIndex = { LOC_GATEHOUSE, LOC_GATEHOUSE_CLOSET },
+
+        .actions    = {
+            {.name = "open door", .execute = act_openDoor}
+        },
+
+
+        .actionCount = 1,
+        .locked = false
+
+        
+    },
+
+    [D5] = {
+        .name = "",
+        .description = "Test desc door 3",
+
+        .targetIndex = { LOC_GATEHOUSE, LOC_GATEHOUSE_GUARDSHACK},
+
+        .actions    = {
+            {.name = "open door", .execute = act_openDoor}
+        },
+
+
+        .actionCount = 1,
+        .locked = false
+
+        
+    },
+
+
+    
 };
 
 static  Item itemPool[ITM_COUNT] = {
@@ -120,14 +251,59 @@ static  Item itemPool[ITM_COUNT] = {
         .itmActionCount = 1
     },
 
-    [KEY_TEST] = {
-        .name = "Key door 1",
-        .description = "This key might be usefull to open the first door.",
+
+    [ITM_GRANNYS_PURSE] = {
+        .name = "The reseptionists handbag.",
+        .text = "A Handbag deeper than any other place known to mankind",
 
         .itemType = ITM_TYPE_TEXT,
 
         .itmActions = {
-            { .name = "Key 1", .execute = itm_satisfy, .token = { .Tkns = {&doorPool[DOOR_START_CHAMBER].sat} } }
+            {.name = "satisfy", .execute = itm_satisfy, .token = { .Tkns = {&personPool[NPC_GRANNY].sat}}}
+        },
+        .itmActionCount = 1
+
+    },
+
+    [ITM_GRANNYS_GLASSES] = {
+        .name = "A pair of glasses",
+        .text = "",
+
+        .itemType = ITM_TYPE_TEXT,
+
+        .itmActions = {
+            {.name = "satisfy", .execute = itm_satisfy, .token = { .Tkns = {&personPool[NPC_GRANNY].sat}}}
+        },
+        .itmActionCount = 1
+
+    },
+
+
+
+
+
+
+
+    [KEY_TEST] = {
+        .name = "Key door 1",
+        .text = "This key might be usefull to open the first door.",
+
+        .itemType = ITM_TYPE_TEXT,
+
+        .itmActions = {
+            { .name = "Key 1", .execute = itm_satisfy, .token = { .Tkns = {&doorPool[ENT_PATH_].sat} } }
+        },
+        .itmActionCount = 1
+    },
+
+    [KEY_GATEH_MAIN] = {
+        .name = "Key ENT_GATEHOUSE_MAIN",
+        .text = "",
+
+        .itemType = ITM_TYPE_TEXT,
+
+        .itmActions = {
+            { .name = "Key main gate", .execute = itm_satisfy, .token = { .Tkns = {&doorPool[ENT_GATEHOUSE_MAIN].sat} } }
         },
         .itmActionCount = 1
     }
@@ -139,8 +315,8 @@ static  Compartment compPool[COMP_COUNT] = {
     [COMP_TEST] = {
         .name = "Broken drawer",
 
-        .items = { &itemPool[ITM_TEST_ZETTEL], &itemPool[KEY_TEST] },
-        .itemCount = 2,
+        .items = { &itemPool[KEY_TEST] },
+        .itemCount = 1,
 
         .locked = false
     }
@@ -192,8 +368,37 @@ static  Person personPool[NPC_COUNT] = {
             }, 
             .satActionCount = 1
         }
-    },  
-    [NPC_GUARD] = { "guard", "A guard keeps his distance." }
+    },
+    [NPC_GUARD] = { "guard", "A guard keeps his distance." },
+
+    [NPC_GRANNY] = {
+        .name = "old receptionist",
+        .description = "A hunchbacked, little and very old resptionist looks at you in distress.",
+
+        .dialog = {
+            {""},
+            {""}
+        },
+
+        .actions = {
+            {.name = "give item", .execute = act_giveItem, .token = { .Tkns = { &personPool[NPC_MINER].wants } } }
+        },
+        .actionCount = 1,
+
+
+        .wants = {&itemPool[ITM_GRANNYS_PURSE], &itemPool[ITM_GRANNYS_GLASSES], NULL},
+        .wantsCount = 2,
+        
+        
+        .sat = {
+            .level = 1, 
+            .satActions = {
+                {.execute = act_take, .token = { .Tkns = { &itemPool[KEY_GATEH_MAIN], } } }
+            }, 
+            .satActionCount = 1
+        }
+
+    }
 };
 
 
@@ -201,8 +406,8 @@ static  Person personPool[NPC_COUNT] = {
 static Location worldData[LOC_COUNT] = {
     [LOC_START] = {
         .tag = "start",
-        .description = "a dark cave",
-        .doors = { &doorPool[DOOR_START_CHAMBER] },
+        .description = "a path in the",
+        .doors = { &doorPool[ENT_PATH_] },
         .doorCount = 1,
         .objects = { &objectPool[OBJ_STONE], &objectPool[OBJ_TABLE] },
         .objectCount = 2,
@@ -210,16 +415,78 @@ static Location worldData[LOC_COUNT] = {
         .personCount = 1
     },
 
-    [LOC_CHAMBER] = {
-        .tag = "chamber",
-        .description = "a torch lit chamber",
-        .doors = { &doorPool[DOOR_START_CHAMBER] },
-        .doorCount = 1,
+    [LOC_MAIN_DRIVEWAY] = {
+        .tag = "driveway",
+        .description = "a large driveway ",
+        .doors = { &doorPool[ENT_PATH_], &doorPool[ENT_DRIVEWAY_GATEHOUSE] },
+        .doorCount = 2,
         .objects = {  },
-        .objectCount = 1,
+        .objectCount = 0,
         .persons = { &personPool[NPC_GUARD] },
         .personCount = 1
+    },
+
+    [LOC_GATEHOUSE] = {
+        .tag = "gatehouse",
+        .description = "an dimly lit gatehouse",
+        .doors = { &doorPool[ENT_DRIVEWAY_GATEHOUSE], &doorPool[ENT_GATEHOUSE_MAIN], &doorPool[ENT_GATEHOUSE_OFFICE] },
+        .doorCount = 3,
+        .objects = {  },
+        .objectCount = 0,
+        .persons = { &personPool[NPC_GRANNY] },
+        .personCount = 0
+
+    },
+
+    [LOC_MAIN_ENTRANCE] = {
+        .tag = "LOC_MAIN_ENTRANCE",
+        .description = "Test room desc 4",
+        .doors = { &doorPool[ENT_GATEHOUSE_MAIN] },
+        .doorCount = 1,
+        .objects = {  },
+        .objectCount = 0,
+        .persons = {  },
+        .personCount = 0
+
+    },
+
+    [LOC_GATEHOUSE_OFFICE] = {
+        .tag = "LOC_GATEHOUSE_OFFICE",
+        .description = "Test room desc 5",
+        .doors = { &doorPool[ENT_GATEHOUSE_OFFICE] },
+        .doorCount = 1,
+        .objects = {  },
+        .objectCount = 0,
+        .persons = {  },
+        .personCount = 0
+
+    },
+
+    [LOC_GATEHOUSE_CLOSET] = {
+        .tag = "LOC_GATEHOUSE_CLOSET",
+        .description = "Test room desc 5",
+        .doors = { &doorPool[ENT_GATEHOUSE_OFFICE] },
+        .doorCount = 1,
+        .objects = {  },
+        .objectCount = 0,
+        .persons = {  },
+        .personCount = 0
+
+    },
+
+
+    [LOC_GATEHOUSE_GUARDSHACK] = {
+        .tag = "",
+        .description = "Test room desc 5",
+        .doors = { &doorPool[ENT_GATEHOUSE_OFFICE] },
+        .doorCount = 1,
+        .objects = {  },
+        .objectCount = 0,
+        .persons = {  },
+        .personCount = 0
+
     }
+    
 };
 
 
@@ -250,7 +517,8 @@ Location *gameCurrentLocation(GameState *gs) {
 
 
 
-void env_handleLook(GameState *gs) {
+void env_handleLook(GameState *gs) 
+{
     Location *current = &gs -> locations[gs->currentLocation];
     
     printf("\nYou look around and see:\n");
@@ -459,10 +727,10 @@ void act_openDoor(GameState *gs, void* prxy, Token* token) {
     if (d -> locked) {
         printf("\n\n\nThis door is locked.\nTry using a key...\n\n\n");
         return;
-    }
+    }   
     else {
-        gs -> currentLocation == d -> targetIndex[0] ? gs -> currentLocation = d -> targetIndex[1] :
-        gs -> currentLocation == d -> targetIndex[1] ? gs -> currentLocation = d -> targetIndex[0] :
+        gs -> currentLocation == d -> targetIndex[0] ? (gs -> currentLocation = d -> targetIndex[1], printf("\n\n\nYou enter %s\n\n\n", gs -> locations[gs -> currentLocation].description )) :
+        gs -> currentLocation == d -> targetIndex[1] ? (gs -> currentLocation = d -> targetIndex[0], printf("\n\n\nYou enter %s\n\n\n", gs -> locations[gs -> currentLocation].description )) :
         printf("You are not supposed to be here! (or get there?)");
         return;
     }
@@ -490,11 +758,7 @@ void act_take(GameState *gs, void* prxy, Token* token) {
 
 
 void act_openComp(GameState *gs, void* prxy, Token* token) {
-
-
-
     
-
     Token *t = (Token*)token;
 
     Compartment *c = t -> Tkns[0];
@@ -504,7 +768,7 @@ void act_openComp(GameState *gs, void* prxy, Token* token) {
         return;
     }
     else {
-        if (c -> itemCount == 0) {printf("%s is empty", c -> name); return;}
+        if (c -> itemCount == 0) {printf("\n\n%s is empty\n\n\n", c -> name); return;}
 
         for (int i = 0; i < c -> itemCount; i++) {
 
@@ -582,7 +846,7 @@ void sat_UnOrLock(GameState *gs, void* prxy, Token* token) {
     bool *locked = t -> Tkns[0];
 
     if (*locked == true){
-        printf("*click*");
+        printf("\n\n\n*click*\n\n\n");
         *locked = false;
     }
     else if (*locked == false){
@@ -611,13 +875,12 @@ void act_giveItem(GameState *gs, void* prxy, Token* token) {
 
             for (int j = 0; j < wantedItems[i] -> itmActionCount; j++) wantedItems[i] -> itmActions[j].execute(gs, prxy, &wantedItems[i] -> itmActions[j].token);
 
-            wantedItems[i] = &itemPool[ITM_LEER];
             return;
         }
         i++;
     }
 
-    printf("\n\n\n%s cannot be used with here.\n\n\n", toggleItem -> name);
+    printf("\n\n\n%s cannot be used here.\n\n\n", toggleItem -> name);
     return;
 
 
@@ -630,11 +893,12 @@ Item* env_openInventory(GameState *gs) {
     printf("\n\n\nYour items:\n");
 
     for (int i = 0; i < gs -> itemsInvCount; i++) {
-        printf("\n%d. %s", i+1, gs -> itemsInv[i] -> name);
+        printf("\n%d. %s:", i+1, gs -> itemsInv[i] -> name);
+        printf("\n%s", gs -> itemsInv[i] -> text);
     }
 
 
-    printf("\nYour Choice: ");
+    printf("\n\nYour Choice: ");
 
     int choice;
     scanf("%d", &choice);
@@ -643,7 +907,7 @@ Item* env_openInventory(GameState *gs) {
     
     
     if (choice <= gs -> itemsInvCount) {
-        printf("\n\n\nJANK!!!: %s", gs -> itemsInv[choice-1] -> text);
+       
         return gs -> itemsInv[choice-1];
     }
 
